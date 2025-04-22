7c6532aad5c505ef99c10196ee1a675c36e22e90
Mercy <mercyemeth@gmail.com>
2025-04-22 12:07:25 -0400

    Adds class to airtable.service and removes token from public repo

--- src/http-common.js
+++ src/http-common.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: "https://api.airtable.com/v0/appG8fsqYsRBUDwHx/",
   headers: {
     Authorization:
-      "Bearer patTF6uztQ1JYeyXj.61b1a82f86b5de52e7b1c3acbbd2665539483848801b678407975f8496651aed",
+      "Bearer ",
     "Content-type": "application/json",
   },
 });
--- src/services/airtable.service.js
+++ src/services/airtable.service.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Airtable from "airtable";
-// Get a base ID for an instance of art gallery example
-var base = new Airtable({ apiKey: 'patTF6uztQ1JYeyXj.61b1a82f86b5de52e7b1c3acbbd2665539483848801b678407975f8496651aed' }).base('appG8fsqYsRBUDwHx');
-export const getATRecords = async (tableName) => {
-  try {
-    const records = await base(tableName).select().all();
-    console.log("AIRTABLE:"+records);
-    return records;
-    //.map(record => ({ id: record.id, ...record.fields }));
-  } catch (error) {
-    console.error("Error fetching records:", error);
-    return [];
+class BugsAirtableService {
+  getAll() {
+    console.log("response");
+    return http.get("/Bugs");
   }
-};
+  // Get a base ID
+  base = new Airtable({
+    apiKey:
+      "",
+  }).base("appG8fsqYsRBUDwHx");
+  getATRecords = async (tableName) => {
+    try {
+      const records = await base(tableName).select().all();
+      console.log("AIRTABLE:" + records);
+      return records;
+      //.map(record => ({ id: record.id, ...record.fields }));
+    } catch (error) {
+      console.error("Error fetching records:", error);
+      return [];
+    }
+  };
+  // Update
+
+  // Delete
+}
+export default new BugsAxiosService();
